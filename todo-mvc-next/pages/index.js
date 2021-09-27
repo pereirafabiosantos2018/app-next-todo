0b3c1d7ba918502b3a75cde3d3bdc5444f9bae14
Fabio Pereira dos Santos <fabio.santos@lawsoft.com.br>
2021-09-27 09:41:01 -0300

    Rodapé da página

--- todo-mvc-next/pages/index.js
+++ todo-mvc-next/pages/index.js
@@ -1,32 +1,51 @@
-import { Button, Row, Col, Image } from 'antd';
+// AntDesign
+import { Row, Col, Image } from 'antd';
+import { HeartTwoTone } from '@ant-design/icons';
 
-// import Image from 'next/image'
+// Next
+import Link from 'next/link'
 
+// Enums
 import { Colors } from '../Enums/enums';
 
 export default function Home() {
 
   return (
 
-    <div style={{ backgroundColor: Colors.Azul, height: 250, width: '100%'}}>
+    <>
+      <div style={{ backgroundColor: Colors.Azul, height: 250, width: '100%'}}>
+          
+          <Row>
         
-        <Row>
-      
-          <Col span={2} offset={6} style={{ marginTop: 50 }}>
-      
-            <Image
-              width={150}
-              height={150}
-              src={'/todo.png'} />
+            <Col span={2} offset={6} style={{ marginTop: 50 }}>
+        
+              <Image
+                width={150}
+                height={150}
+                src={'/todo.png'} />
 
-          </Col>
+            </Col>
 
-          <Col span={8} offset={1}>
-            <p style={{ marginTop: 100, fontSize: 40, color: Colors.Branco }}>Fabin TodoMVC</p>
-          </Col>
+            <Col span={8} offset={1}>
+              <p style={{ marginTop: 100, fontSize: 40, color: Colors.Branco }}>Fabin TodoMVC</p>
+            </Col>
 
-        </Row>
+          </Row>
 
-    </div>
+      </div>
+
+      <Row>
+        
+        <Col span={10} offset={7} style={{ bottom: 20, position: 'absolute' }}>
+          Feito com  <HeartTwoTone twoToneColor="#eb2f96" /> por Fabin Pereira dos Santos. Based on 
+
+          <Link href="https://github.com/tastejs/todomvc/blob/master/app-spec.md#functionality">
+            <a target={'_blank'}>{' '} TodoMVC functionality</a>
+          </Link>
+
+        </Col>
+
+      </Row>
+    </>
   )
 }
